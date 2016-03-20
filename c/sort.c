01385c856f0d5e8ea2b9a981344762c0e0f56001
SkipperQian <skipper.qian@gmail.com>
2016-03-20 22:46:46 +0800

    Fix an error of bubble sort. Add select sort function

--- c/sort.c
+++ c/sort.c
@@ -1,35 +1,64 @@
 #include <stdio.h>
 
-void exchange(int *a, int *b)
+void display_array(int array[], int size)
 {
-    int tem;
-
-    tem = *a;
-    *a = *b;
-    *b = tem;
-}
-
-void bubble(int array[], int size)
-{
-    int i, j;
-    int tmp = 0;
+    int i;
 
     for (i = 0; i < size; i++) {
-        for (j = 0; j < size - 1; j++) {
-            if (array[j] > array[j+1]) {
-                tem = array[j];
-                array[j] = array[j + 1];
-                array[j + 1] = tem;
+        printf("%d ", array[i]);
+    }
+    printf("\n");
+}
+
+void exchange(int *a, int *b)
+{
+    int temp;
+
+    temp = *a;
+    *a = *b;
+    *b = temp;
+}
+
+void sort_bubble(int array[], int size)
+{
+    int i, j;
+
+    for (i = 0; i < size - 1; i++) {
+        for (j = size - 1; j > i ; j--) {
+            if (array[j] < array[j - 1]) {
+                exchange(&array[j], &array[j - 1]);
             }
         }
+    }
+}
+
+void sort_select(int array[], int size)
+{
+    int i, j;
+    int key_index;
+
+    for (i = 0; i < size - 1; i++) {
+        key_index = i;
+        for (j = i + 1; j < size; j++) {
+            if (array[j] < array[key_index]) {
+                key_index = j;
+            }
+        }
+
+        if (i != key_index) 
+            exchange(&array[key_index], &array[i]);
     }
 }
 
 int main(int argc, char *argv[])
 {
     int array1[10] = {7, 4, 1, 2, 5, 8, 9, 6, 3, 0};
+    int array2[10] = {7, 4, 1, 2, 5, 8, 9, 6, 3, 0};
 
-    bubble(array1, 10);
+    sort_bubble(array1, 10);
+    display_array(array1, 10);
 
+    sort_select(array2, 10);
+    display_array(array2, 10);
     return 0;
 }
