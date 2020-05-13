4df0db0ae827a2eb207e43baaa8cfa65450fbd3a
SkipperQian <skipper.qian@outlook.com>
2020-05-13 17:28:53 +0800

    my_genl: add generic netlink driver test code

---
+++ kern/nl/sample/genl_echo.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <sys/socket.h>
+#include <errno.h>
+#include <linux/genetlink.h>
+
+int main(int argc, char *argv[])
+{
+
+	return 0;
+}
